3c623d822e3e3259ec145e81d7e405665165bb7f
Sul <sharifsuliman1@gmail.com>
2024-08-06 11:07:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ from stmol import showmol
 
 input = st.text_area("Enter Your Z-Matrix Coordinate Data Here Alex:")
 
-def read_xyz_file(file):
-    return file.getvalue().decode("utf-8")
-
 def render_mol(xyz):
-    view = py3Dmol.view(width=800, height=400)
+    view = py3Dmol.view(width=400, height=400)
     view.addModel(xyz, "xyz")
     view.setStyle({'stick': {}})
+    view.setBackgroundColor('black')
     view.zoomTo()
     return view
 
@@ -21,18 +19,16 @@ if input:
     dimer.save_xyz_file('test.xyz', False)
 
     with open('test.xyz') as in_file:
-        file = in_file.read()
+        xyz_data = in_file.read()
 
     st.text_area(label="Output Coordinate:", value=file, height=550)
 
     st.title("XYZ Molecule Viewer")
-    
-    xyz_data = read_xyz_file('test.xyz')
-    
+        
     # Render the molecule
     st.subheader("3D Molecule Viewer:")
     view = render_mol(xyz_data)
-    showmol(view, height=400, width=800)
+    showmol(view, height=400, width=400)
         
     st.download_button(
       label="Download XYZ file",
